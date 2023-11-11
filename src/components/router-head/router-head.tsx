fd829f19e972a0c90a42032984ee29b09651e16a
encode42 <me@encode42.dev>
2023-11-11 15:50:31 -0500

    Favicon

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,12 +15,19 @@ export const RouterHead = component$(() => {
             <meta property="og:title" content={title}/>
             <meta property="og:description" content={description}/>
             <meta name="theme-color" content="#cad3f5"/>
+            <meta name="msapplication-TileColor" content="#603cba"/>
             <link rel="canonical" href={location.url.href}/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
             {head.meta.map(meta => (
                 <meta key={meta.key} {...meta}/>
             ))}
+            <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+            <link rel="manifest" href="/favicon/site.webmanifest"/>
+            <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#24273a"/>
+            <meta property="og:image" content="https://erora.live/badge.png"/>
             {head.links.map(link => (
                 <link key={link.key} {...link}/>
             ))}
